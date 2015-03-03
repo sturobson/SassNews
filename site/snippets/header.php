69f8c5048d45bef3494e757915ce68d2b2ab4a49
Stu Robson <stu@alwaystwisted.com>
2015-03-03 07:51:55 +0000

    adds PNG fallback for IE8

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="no-svg">
 <head>
 
   <meta charset="utf-8" />
@@ -8,6 +8,15 @@
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+  <script>
+  if (document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
+    document.documentElement.className = "";
+    document.documentElement.className = "svg";
+  }
+  else {
+
+  }
+  </script>
   <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
   <?php echo css('assets/css/main.css') ?>
 
@@ -16,7 +25,7 @@
 
   <header class="l-header cf" role="banner">
     <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
+      Sass News
     </a>
     <?php snippet('menu') ?>
   </header>
